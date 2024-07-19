60983a1802e980383a1c81be583519b37a62378b
mahdiabouzeid <mahdiabouzeid602@gmail.com>
2024-07-19 19:41:36 +0300

    profile page finished

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,18 +1,76 @@
 import 'package:flutter/material.dart';
-class Profile extends StatefulWidget {
+import 'profile_class.dart';
+class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
-  State<Profile> createState() => _ProfileState();
-}
-
-class _ProfileState extends State<Profile> {
-  @override
   Widget build(BuildContext context) {
-    return SafeArea(child:  
-    Scaffold(
-      body:Text('Profile',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
-    ),
+    return Scaffold(
+       appBar: AppBar(
+        
+        automaticallyImplyLeading: false,
+        title: Text('Profile'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage('https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o='),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                profile1.username,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold ,color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text(
+                profile1.email,
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 20),
+          Divider(),
+          ListTile(
+              leading: Icon(Icons.music_note),
+              title: Text('Your Songs',style: TextStyle(color: Colors.white),),
+              
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Favorites',style: TextStyle(color: Colors.white)),
+             
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings',style: TextStyle(color: Colors.white)),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+            Text(
+              'Current Plan:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,color: Colors.white),
+            ),
+            SizedBox(width: 5.0,),
+            Text(
+              profile1.payedplan ? 'Paid Plan' : 'Free Plan',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          ]
+            )
+
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/profile_class.dart
@@ -0,0 +1,8 @@
+class ProfileClass{
+ String username;
+ String email;
+bool payedplan;
+ ProfileClass(this.username,this.email,this.payedplan);
+
+}
+ProfileClass profile1=ProfileClass('johnD', 'Example@gmail.com', false);
